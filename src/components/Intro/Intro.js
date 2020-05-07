75fee4a86fe325de3de07c42fbf6b88998d6abcc
oliverfeher <oliver92.feher@gmail.com>
2020-05-07 09:25:45 -0400

    Create NavBar component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Intro.css"
 
 class Intro extends React.Component
 {
     render()
     {
         return (
-            <div>
-                <p>my name is <span>Oliver</span></p>
-                <p><span>I'm a</span>Software Engineer</p>
+            <div id="intro-container">
+                <p className="intro-p">my name is <span>Oliver</span></p>
+                <p className="intro-p"><span>I'm a </span>Software Engineer</p>
             </div>
         );
     }
