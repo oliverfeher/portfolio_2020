68aad1773c691d55e3118222fa010a329645520a
oliverfeher <oliver92.feher@gmail.com>
2020-05-07 18:40:25 -0400

    Import ReactRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,23 +6,45 @@ import NavBar from "../NavBar/NavBar"
 import Dots from "../Dots/Dots";
 import Button from "../Button/Button";
 import SocialIcons from "../SocialIcons/SocialIcons";
+import { Switch, Link, Route } from "react-router-dom";
 
-class App extends React.Component 
+function App() 
 {
-    render()
-    {
-        return (
-            <div id="main">
-                <Dots place="top-left"/>
-                <Dots place="bottom-right"/>
-                <NavBar />
+    return (
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutMe} />
+        </Switch>
+    )
+}
+
+function HomePage()
+{
+    return (
+        <div id="main">
+            <Dots place="top-left"/>
+            <Dots place="bottom-right"/>
+            <NavBar />
+            <div id="center">
                 <Intro />
                 <Hello />
                 <Button id="explore-btn" title="explore"/>
-                <SocialIcons />
             </div>
-        );
-    }
+            <SocialIcons />
+        </div>
+    )
+}
+
+function AboutMe()
+{
+    return (
+        <div>
+            Hello About me!
+        </div>
+    )
 }
 
 export default App;
+
+
+
