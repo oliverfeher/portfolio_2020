ba38fe84d23ab40f3295334737d22bb6b7458095
oliverfeher <oliver92.feher@gmail.com>
2020-05-08 15:43:16 -0400

    Add conditional rendering with state
    - isClicked false/true

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,14 +2,34 @@ import React from "react";
 
 class ProjectCard extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = { isClicked: false};
+    }
+
     render()
     {
-        return (
-            <div className="card-container">
-                <img class="card-img" src={this.props.src}></img>
-            </div>
-        )
+        const isClicked = this.state.isClicked;
+
+        if(!isClicked) 
+        {
+            return (
+                <div className="card-container">
+                    <img class="card-img" src={this.props.src}></img>
+                </div>
+            )
+        }
+        else
+        {
+            return (
+                <div className="card-container">
+                    <div>Hello</div>
+                </div>
+            )
+        }
     }
+
 }
 
 export default ProjectCard;
